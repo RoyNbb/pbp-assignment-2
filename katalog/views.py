b0b32d4f84d7b615d19328002c4304e02a2c0f2c
Roy Maruli Tua Nababan <roy.maruli@ui.ac.id>
2022-09-14 10:12:24 +0700

    assignment 2 first try

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def index(request):
+    catalogs = CatalogItem.objects.all()
+    context = {
+        'list_catalog': catalogs ,
+        'name' : 'Roy Maruli Tua Nababan',
+        'id' : '2106750521'
+    }
+    return render(request, 'katalog.html', context)
